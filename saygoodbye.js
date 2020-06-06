c27ee4146e35c3b3de3e07cfd61593f69d999012
prachiigoyal <66202122+prachiigoyal@users.noreply.github.com>
2020-06-06 17:30:47 +0530

    Add files via upload

---
+++ saygoodbye.js
@@ -0,0 +1,11 @@
+(function(window){
+	var byespeaker={};
+	var speakword = "Goodbye";
+
+	byespeaker.speak = function(name)
+	{
+		console.log(speakword + " " + name);
+	}
+	window.byespeaker = byespeaker;
+
+})(window);
